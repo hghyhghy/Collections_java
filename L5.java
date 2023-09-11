1a36ea496f2944d5525d8025a546f1a9279583a1
Subham Sarkar <140393712+hghyhghy@users.noreply.github.com>
2023-09-11 22:26:53 +0530

    Add files via upload

---
+++ L5.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+
+public class L5
+ {
+        public static void main(String[] args)
+         {
+              
+                 // creating an object a in class ArrayList
+
+                 ArrayList <String> a= new ArrayList<>();
+
+              a.add("Subham");
+                              a.add("Souren");
+
+                                             a.add("Sourendra");
+
+                                                            a.add("Saikat");
+
+                                                        a.add("Sagnik");
+
+         // traversing through the list by using for each loop
+         
+         for (String   fruit : a)
+
+
+          {
+               System.out.println(fruit);
+         }
+
+        }
+}
